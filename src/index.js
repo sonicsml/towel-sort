77440719e9fa4e46aaed6ff81a452c53197d546f
Polovkov Dmitry <sonics.scs@gmail.com>
2020-03-22 16:54:49 +0300

    feat: solution for stak towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix != undefined) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 != 0) {
+        matrix[i] = matrix[i].toString().split(',').reverse(); 
+      } else {
+        matrix.flat();
+      }
+    }
+    return( matrix.flat(Infinity) );
+  } else return [];
 }
